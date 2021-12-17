557df628f7ada2c5bf5b432de43fc7ecb1aac40c
Don Gumayagay <dngumayagay@gmail.com>
2021-12-18 01:25:48 +0800

    check changing of background

--- src/lib/Options.svelte
+++ src/lib/Options.svelte
@@ -25,7 +25,7 @@
   {#if choices.length > 0}
     <div
       transition:fade
-      class="absolute inset-0 w-full h-full bg-black/30 flex flex-col items-center justify-center space-y-2"
+      class="absolute inset-0 w-full h-full bg-black/20 flex flex-col items-center justify-center space-y-2"
     >
       {#each choices as choice}
         <button
@@ -33,7 +33,7 @@
             currentScene = scenes[choice.nextScene];
             choices = [];
           }}
-          class=" bg-gradient-to-r from-transparent via-black/50 to-transparent text-white w-1/2 py-1"
+          class=" bg-gradient-to-r from-transparent via-black/50 to-transparent text-white w-1/2 py-1 tracking-tight font-light leading-5"
           >{choice.text}</button
         >
       {/each}
